9d821cadc246a065ec6cc96028e8ebd6af3143cc
Ondrej Mladek <ondrej.mladek@actum.cz>
2016-11-02 13:46:45 +0100

    * refactoring

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
-let lastNotificationText = '';
-let unreadEmailsTotal = getUnreadEmailsTotal();
-let lastNotificationId = getNotificationId();
+const appConfig = {
+    debug: false,
+    notificationIcon: 'http://www.clker.com/cliparts/E/C/p/S/0/F/exclamation-mark-red-md.png',
+    ignoredFolders: [
+        'CMS hillspet',
+        'Odstraněná pošta',
+        'newslettery'
+    ]
+};
 
-console.log('lastNotificationText: ' + lastNotificationText);
-console.log('unreadEmailsTotal: ' + unreadEmailsTotal);
-console.log('lastNotificationId: ' + lastNotificationId);
+let LAST_NOTIFICATION_TEXT = '';
+let UNREAD_EMAILS_TOTAL = getUnreadEmailsTotal();
+let LAST_NOTIFICATION_ID = getNotificationId();
+
+if (appConfig.debug) {
+    console.log('LAST_NOTIFICATION_TEXT: ' + LAST_NOTIFICATION_TEXT);
+    console.log('UNREAD_EMAILS_TOTAL: ' + UNREAD_EMAILS_TOTAL);
+    console.log('LAST_NOTIFICATION_ID: ' + LAST_NOTIFICATION_ID);    
+}
 
 function notifyMe() {
-    console.log('notify');
+    if (appConfig.debug) {
+        console.log('notify');
+    }
+
     if (Notification.permission !== 'granted') {
         Notification.requestPermission();
     } else {
         const notification = new Notification('New mail:', {
-            icon: 'http://www.clker.com/cliparts/E/C/p/S/0/F/exclamation-mark-red-md.png',
-            body: lastNotificationText,
+            icon: appConfig.notificationIcon,
+            body: LAST_NOTIFICATION_TEXT,
         });
 
         notification.onclick = function() {
-            document.querySelector('.o365cs-notifications-notificationPopupArea .o365cs-notifications-newMailLink').click();
+            window.focus();
         };
     }
 }
@@ -43,13 +58,8 @@ function getUnreadEmailsTotal() {
  */
 function isIgnoredFolder(el) {
     let isIgnored = false;
-    const ignoredFolders = [
-        'CMS hillspet',
-        'Odstraněná pošta',
-        'newslettery'
-    ];
 
-    ignoredFolders.forEach((item) => {
+    appConfig.ignoredFolders.forEach((item) => {
         if (el.parentNode.querySelector(`[title="${ item }"]`)) {
             isIgnored = true;
         }
@@ -73,54 +83,65 @@ function getLatestEmailText() {
  * @param  {integer} currentUnread
  */
 function getNotificationId(currentUnread) {
-    let count = unreadEmailsTotal + lastNotificationText;
+    let count = UNREAD_EMAILS_TOTAL + LAST_NOTIFICATION_TEXT;
 
     if (currentUnread) {
-        count = currentUnread + lastNotificationText;
+        count = currentUnread + LAST_NOTIFICATION_TEXT;
     }
 
     return count;
 }
 
 // select the target node
-const observerTarget = window.document.querySelector('[aria-label="Podokno složek"]');
+const observerTarget = window.document.querySelector('.treeHeaderContainer + div[role=tree]');
 
 // create an observer instance
-const observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
+const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
         handleMutationChange();
     });
 });
 
 function handleMutationChange() {
-    let currentMailSubject = getLatestEmailText();
-    let currentUnreadEmailsTotal = getUnreadEmailsTotal();
-    let canContinue = false;
-    let newNotificationId = getNotificationId(currentUnreadEmailsTotal);
+    const currentMailSubject = getLatestEmailText();
+    const currentUnreadEmailsTotal = getUnreadEmailsTotal();
+    const newNotificationId = getNotificationId(currentUnreadEmailsTotal);
+    const newState = {
+        currentMailSubject: currentMailSubject,
+        currentUnreadEmailsTotal: currentUnreadEmailsTotal,
+        notificationId: getNotificationId()
+    };
 
-    console.log('observe');
-    console.log('unreadEmailsTotal:' + unreadEmailsTotal);
-    console.log('currentUnreadEmailsTotal:' + currentUnreadEmailsTotal);
-    console.log('newNotificationId:' + newNotificationId);
-    console.log('lastNotificationId:' + lastNotificationId);
+    if (appConfig.debug) {
+        console.log('observe');
+        console.log('UNREAD_EMAILS_TOTAL:' + UNREAD_EMAILS_TOTAL);
+        console.log('currentUnreadEmailsTotal:' + currentUnreadEmailsTotal);
+        console.log('newNotificationId:' + newNotificationId);
+        console.log('LAST_NOTIFICATION_ID:' + LAST_NOTIFICATION_ID);        
+    }
 
     // in case I am reading the unread email
-    if (currentUnreadEmailsTotal < unreadEmailsTotal) {
-        lastNotificationText = currentMailSubject;
-        unreadEmailsTotal = currentUnreadEmailsTotal;
-        lastNotificationId = getNotificationId();
+    if (currentUnreadEmailsTotal < UNREAD_EMAILS_TOTAL) {
+        setState(newState);
         return;
     }
 
     // in case a new mail arrived - show just one notification
-    if (newNotificationId !== lastNotificationId) {
-        if (currentUnreadEmailsTotal > unreadEmailsTotal && unreadEmailsTotal !== currentUnreadEmailsTotal) {
-            lastNotificationText = currentMailSubject;
-            unreadEmailsTotal = currentUnreadEmailsTotal;
-            lastNotificationId = getNotificationId();
+    if (newNotificationId !== LAST_NOTIFICATION_ID) {
+        if (currentUnreadEmailsTotal > UNREAD_EMAILS_TOTAL && UNREAD_EMAILS_TOTAL !== currentUnreadEmailsTotal) {
+            setState(newState);
             notifyMe();
         }
     }
+}
+
+/**
+ * @param {object} state
+ */
+function setState(state) {
+    LAST_NOTIFICATION_TEXT = state.currentMailSubject;
+    UNREAD_EMAILS_TOTAL = state.currentUnreadEmailsTotal;
+    LAST_NOTIFICATION_ID = state.notificationId;
 }
 
 // configuration of the observer:
@@ -129,7 +150,7 @@ const observerConfig = {
     childList: true,
     characterData: true,
     subtree: true
-}; 
+};
 
 // pass in the target node, as well as the observer options
 observer.observe(observerTarget, observerConfig);
